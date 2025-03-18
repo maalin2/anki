b3832fa6d82c37cb7a3eb4e55630d396758423bf
mo <1maalinma@gmail.com>
2025-03-17 22:38:35 -0400

    gemini prompt; clean up code

--- read_pdf.py
+++ read_pdf.py
@@ -1,10 +1,11 @@
+import asyncio
+import base64
 import os
-from dotenv import load_dotenv
 import pymupdf
-
-def ask_gemini():
-    ""ask nicely"""
-    return 'i dont know'
+from time import sleep
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 def read_pdf(doc):
     """read pages as images"""
@@ -14,22 +15,48 @@ def read_pdf(doc):
         img = p.get_pixmap()
         # TODO: parameterize folder, path name to organize slide decks 
         path = f'./img/page-{i}.png'
-        img.save(path)
+        try:
+            img.save(path)
+        except Exception as e:
+            print(f'error {e}')
+
         paths.append(path)
-        print(f'saved {path}')
 
     return paths
+
+def ask_gemini(client, p, key):
+    """ask nicely"""
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-exp',
+        contents=['whats going on here',
+            types.Part(
+                inline_data=types.Blob(data=p, mime_type='image/png')
+            )
+        ]
+    )
+
+    print(response.text)
+
+def run_agent(client, pages, key):
+    """run queries"""
+    for p in pages:
+        img = open(p, 'rb').read()
+        ask_gemini(client, img, key)
+
+        # dont want to get rate limited
+        sleep(2) 
     
 def main():
-    # get from .env file
-    path = os.environ.get('FILE_PATH')
+    # .env file
     key = os.environ.get('GEMINI_KEY')
-    pages = []
+    path = os.environ.get('FILE_PATH')
 
     doc = pymupdf.open(path)
     pages = read_pdf(doc)
-    for p in pages:
-        ask_gemini(p)
+
+    client = genai.Client(api_key=key)
+
+    run_agent(client, pages, key)
 
 if __name__ == '__main__':
     load_dotenv()
